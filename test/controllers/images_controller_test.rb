de804676396c5b4b4459765bbab005847240094e
Andy Vaughn <andy.vaughn@appfolio.com>
2021-03-09 12:52:28 -0800

    move images controller-related tests out of application controller tests

--- test/controllers/images_controller_test.rb
+++ test/controllers/images_controller_test.rb
@@ -1,6 +1,19 @@
 require 'test_helper'
 
 class ImagesControllerTest < ActionDispatch::IntegrationTest
+  test 'can get thumbnails container for thumbnails react component' do
+    get root_url
+    assert_response :success
+    assert_select 'div/@id', 'thumbnails-container'
+  end
+
+  test 'can navigate to submit image form from homepage' do
+    get root_url
+    assert_response :success
+    assert_select '.navbar .btn', 'Submit an Image'
+    assert_select '.navbar a[href=?]', '/images/new'
+  end
+
   test 'images index redirects home' do
     get images_url
     assert_redirected_to root_url
